3c61bbf6b1462890ea7fd22efe3ebe838a8ee43d
Anonymous545-lab <briadonfielies123@gmail.com>
2025-01-26 08:17:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Navbar = () => (
         </h1>
         <ul className="navbar-nav">
             <li><a href="Home.html">Home</a></li>
-            <li><a href="Findings.html">Info</a></li>
+            <li><a href="Info.html">Info</a></li>
         </ul>
     </div>
 );
@@ -44,10 +44,25 @@ const BackgroundVideo = () => {
     );
 };
 
+const Introduction = () => (
+    <div className="content" id="fade-in-paragraph">
+        <h2>Introduction</h2>
+        <p>I am Briadon Fielies, a dedicated and motivated high school student with a strong academic record and a keen interest in IT, software development, software engineering, ethical hacking, and cybersecurity. My hobbies include coding, pentesting, and working on computers for fun. I also enjoy the sense of freedom at the beach. My passion for these fields drives me to seek opportunities for growth and learning. I am eager to apply my skills and enthusiasm to contribute meaningfully to projects and initiatives in the technology sector.</p>
+    </div>
+);
+
+const Footer = () => (
+    <footer>
+        <p>Contact: <a href="mailto:briadonfielies321@gmail.com" className="glow">briadonfielies321@gmail.com</a></p>
+    </footer>
+);
+
 const App = () => (
     <div>
         <Navbar />
         <BackgroundVideo />
+        <Introduction />
+        <Footer />
     </div>
 );
 
